69ff10e8d745e82c5fd606a9cb59ac35be9f2958
Prathmesh <pcshedge@gmail.com>
2023-01-12 22:57:05 +0530

    solution commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String [] args) {
+        int x = 10,y=20,z=30;
+
+        double a = 10, b=20;
+
+        Product p = new Product();
+        System.out.println(p.product(x, y));
+        System.out.println(p.product(a,b));
+        System.out.println(p.product(x,y,z));
+    }
+}
+class Product{
+    public  int product(int x, int y){
+        return x*y;
+    }
+    public  double product(double x, double y){
+        return x*y;
+    }
+    public  int product(int x, int y, int z){
+        return x*y*z;
+    }
 }
